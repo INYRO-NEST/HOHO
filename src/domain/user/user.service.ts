6ef051e42c689729775963787206df0246405ca1
Kim Tae Ho <kok8454@gmail.com>
2023-05-09 01:05:40 +0900

    modify service for login

--- src/domain/user/user.service.ts
+++ src/domain/user/user.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserEntity } from "src/entities/user.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class UserService {
-    constructor () {}
+    constructor (
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>
+    ) {}
 
     async getUserPage() {
         return { user : {
-            age : 12,
             name : "hansu",
-            height : 180,
-            paper : true
+            age : 20,
         }};
+    }
+
+    async register(email: string, password: string) {
+        const user = await this.userRepository.save({
+            email: email,
+            password: password,
+        });
+        return user;
     }
 }
