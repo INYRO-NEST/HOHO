73ade4050b4bea8f271b590415c1aced30ca98bc
Kim Tae Ho <kok8454@gmail.com>
2023-05-09 01:06:28 +0900

    setting for using repository

--- src/domain/user/user.module.ts
+++ src/domain/user/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserEntity } from "src/entities/user.entity";
 
 
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forFeature([UserEntity])], // for root에서 userentity에 해당하는 table을 쓰겠다.
     controllers: [UserController],
     providers: [UserService],
 })
