d117277199cbe2291a6bb85aba42de8c862a2ef4
Kim Tae Ho <kok8454@gmail.com>
2023-05-09 01:04:17 +0900

    cross-env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,39 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './domain/user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path'
+import { ArticleModule } from './domain/article/article.module';
+
+console.log(`.env.${process.env.NODE_ENV}`);
 
 @Module({
-  imports: [UserModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: `.env.${process.env.NODE_ENV}`,
+  }),
+  TypeOrmModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      retryAttempts: configService.get('NODE_ENV') === 'prod' ? 10 : 1,
+      type: 'mysql',
+      host: configService.get('DB_HOST'),
+      port: Number(configService.get('DB_PORT')),
+      database: configService.get('DB_NAME'),
+      username: configService.get('DB_USER'),
+      password: configService.get('DB_PASSWORD'),
+      entities: [
+        path.join(__dirname, 'src/entities/**/*.entity.ts'),
+        path.join(__dirname, 'src/entities/**/*.entity.js'),
+      ],
+      synchronize: false,
+      logging: true,
+      timezone: 'local',
+    })
+  }),
+  UserModule, ArticleModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
